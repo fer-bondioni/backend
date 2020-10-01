b708c3b9eee77401683e42ba6e7f040cf5cc360a
Fernando Bondioni <fernandobondioni@gmail.com>
2020-10-01 09:01:41 -0300

    corrección middlewares cursos y categorias

--- middlewares/schemas/cursos.js
+++ middlewares/schemas/cursos.js
@@ -3,17 +3,16 @@ const { schema } = require("../../utils/bd");
 const schemaId = Joi.number().integer().positive().required();
 
 const messageNombre = {
-    "any.required": "El campo nombre es obligatorio",
-    "string.min": "El campo nombre debe tener al menos dos caracteres",
-    "string.max": "El campo nombre no debe tener más de 30 caracteres",
-  };
+  "any.required": "El campo nombre es obligatorio",
+  "string.min": "El campo nombre debe tener al menos dos caracteres",
+  "string.max": "El campo nombre no debe tener más de 30 caracteres",
+};
 
 const schemas = {
-    create = Joi.object().keys({
-        idCategoria = schemaId,
-        nombre = Joi.string().min(2).max(30).required().messages(messageNombre),
-    })
-}
+  create: Joi.object().keys({
+    idCategoria: schemaId,
+    nombre: Joi.string().min(2).max(30).required().messages(messageNombre),
+  }),
+};
 
 module.exports = { schemas };
-
