1d23760cb3a27d8769910a6b257590c560118ff9
Fernando Bondioni <fernandobondioni@gmail.com>
2020-11-29 22:56:27 -0300

    primero

--- routes/docentes.js
+++ routes/docentes.js
@@ -5,13 +5,32 @@ const multer = require("multer");
 const config = { dest: "./public/tmp" }; //crea la carpeta de modo automatica
 const upload = multer(config);
 
-const all = (req, res) => {};
-
-const create = (req, res) => {
-  console.log(req.files);
+const all = (req, res) => {
+  service
+  .getAll()
+  .then((r)=>res.json(r))
+  .catch((e)=>res.status(500).json(e));
 };
 
-router.get("/", all);
+// .tmp
+// req -> body, params, query, files, session
+
+// Insertar un docente en la tabla
+// Tenemos que almacenar la imagen del docente
+// Tenemos que subir la imagen del docente a aws
+
+const create = (req, res) => {
+  try{
+    const result = service.createDocente(req.body, req.file);
+    res.json({result});
+  }
+  catch{
+    res.sendStatus(500);
+  }
+  // console.log(req.files);
+};
+
+router.get("/all", all);
 // router.get("/single/:id", single);
 router.post("/create", upload.array("imagen"), create);
 // router.put("/modify/:id", modify);
--- services/docentes.js
+++ services/docentes.js
@@ -1,10 +1,56 @@
-const model = require("./../models/docentes");
-const { imgFile } = require("../utils/fileHandler");
+const { create, createImages } = require("./../models/docentes");
+const { imgFile } = require("./../utils/fileHandler");
+/* 
+const createDocente = async (body,files) => {
+  try {
+    const [idDocente] = await create(body);
+    // [Promise, Promise, Promise]
+    const results = files.map(file => {
+      const uid = imgFile(files);
+      const obj = {
+        idDocente,
+        uid,
+      };
+      createImages(obj); // [10]
+    })
+    return await Promise.all(results)
+    return returnImg
+  } catch (e){
 
-const createDocente = async (bodyObj, fileObj) => {
-  const [idDocente] = await model.create(bodyObj);
-  // imgFile
+  }
+}
+*/
 
-  const uuidImage = imgFile(fileObj);
-  // INsertar la imagen del docente
+// while, for, do while, forEach
+const createDocente = async (body, files) => {
+  try {
+    const [idDocente] = await create(body);
+    const results = files.map((file) => {
+      const uid = imgFile(files);
+      const obj = {
+        idDocente,
+        uid,
+      };
+      const [idImagen] = createImages(obj); // [10]
+    });
+    return idImagen;
+  } catch (e) {
+    throw e;
+  }
+  // insertar en la tabla docente_imagenes el obj
 };
+
+module.exports = { createDocente };
+
+
+
+// const model = require("./../models/docentes");
+// const { imgFile } = require("../utils/fileHandler");
+
+// const createDocente = async (bodyObj, fileObj) => {
+//   const [idDocente] = await model.create(bodyObj);
+//   // imgFile
+
+//   const uuidImage = imgFile(fileObj);
+//   // INsertar la imagen del docente
+// };
